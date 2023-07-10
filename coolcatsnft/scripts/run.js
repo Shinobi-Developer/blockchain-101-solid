ac730737c69b919bb44e21bc3351a01a5466fee7
Shinobi <92864027+shinobi8894@users.noreply.github.com>
2023-07-10 07:49:50 -0400

    Init Blockchain 101 solid

---
+++ coolcatsnft/scripts/run.js
@@ -0,0 +1,35 @@
+const main = async () => {
+  const ContractFactory = await hre.ethers.getContractFactory(
+    "CoolCatsTemplate"
+  );
+
+  // TODO: Change to where you will host your metadata
+  const baseURI = "https://api.example.com";
+
+  const contract = await ContractFactory.deploy({ args: [baseURI] });
+  await contract.deployed();
+  console.log("Contract deployed to:", contract.address);
+
+  // Activate minting
+  let activateTxn = await contract.pause(false);
+  await activateTxn.wait();
+
+  // Call the function.
+  let adoptTxn = await contract.adopt(20, {
+    value: hre.ethers.utils.parseEther("1.2"),
+  });
+  // Wait for it to be mined.
+  await adoptTxn.wait();
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
---
+++ coolcatsnft/test/test.js
@@ -0,0 +1,25 @@
+const { ethers } = require("hardhat");
+
+describe("CoolCatsTemplate", function () {
+  it("Should return mint 20 NFTs", async function () {
+    const Contract = await ethers.getContractFactory("CoolCatsTemplate");
+
+    // TODO: Change to where you will host your metadata
+    const baseURI = "https://api.example.com";
+
+    const contract = await Contract.deploy({ args: [baseURI] });
+    await contract.deployed();
+
+    // Activate minting
+    let setActivateTx = await contract.pause(false);
+    await setActivateTx.wait();
+
+    // Test adopting 20 cats
+    const setAdoptTx = await contract.adopt(20, {
+      value: hre.ethers.utils.parseEther("1.2"),
+    });
+
+    // wait until the transaction is mined
+    await setAdoptTx.wait();
+  });
+});
